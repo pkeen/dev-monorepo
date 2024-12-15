401f0a0f28bbb8ba525a805242ef56f720384f79
pkeen <pkeen7@gmail.com>
2024-12-15 01:59:13 -0700

    mmm

--- packages/authentication-core/src/core/index.ts
+++ packages/authentication-core/src/core/index.ts
@@ -2,3 +2,5 @@ export * from "./types";
 export * from "./auth-system";
 export * from "./strategy";
 export * from "./session-state-storage";
+export * from "./utils";
+export * from "./token-service";
--- packages/authentication-core/src/core/session-state-storage/session-state-storage.ts
+++ packages/authentication-core/src/core/session-state-storage/session-state-storage.ts
@@ -5,27 +5,4 @@ export interface SessionStateStorage {
 	retrieve(request?: any): Promise<KeyCards | null>;
 	clear(response?: any): Promise<void>;
 	// create(key: string, value: string, response?: any): Promise<void>;
-	// delete(key: string, response?: any): Promise<void>;
 }
-
-// export abstract class AbstractSessionStateStorage
-// 	implements SessionStateStorage
-// {
-// 	// storing and retrieving auth state
-// 	// I want to have a method defined here that will take take an ImprovedAuthState object and store type and store a cookie for each property
-// 	// Each AuthToken in ImprovedAuthState.tokens should be stored as a cookie
-// 	protected async storeAuthState(
-// 		authTokens: AuthTokens,
-// 		response?: any
-// 	): Promise<void> {
-// 		for (const [key, value] of Object.entries(authTokens)) {
-// 			await this.create(key, value, response);
-// 		}
-// 	}
-
-// 	// protected abstract createCookieOrHeader(
-// 	// 	key: string,
-// 	// 	value: string,
-// 	// 	response?: any
-// 	// ): Promise<void>;
-// }

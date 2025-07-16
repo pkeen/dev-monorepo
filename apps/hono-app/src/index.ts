cb40d1ab487b0345292ba1f7250e81387d9dd6ab
pkeen <pkeen7@gmail.com>
2025-07-16 12:40:24 +0100

    create test nested mono-repo

--- apps/hono-app/src/index.ts
+++ apps/hono-app/src/index.ts
@@ -1,10 +1,11 @@
 import { Hono } from "hono";
-import { hello } from "@pete_keen/test-repo";
+// import { hello } from "@pete_keen/test-repo";
+import hello from "@pete_keen/courses-test";
 
 const app = new Hono();
 
 app.get("/", (c) => {
-	return c.text(hello("Peter!"));
+	return c.text(hello("Peter"));
 });
 
 export default app;
